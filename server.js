75ab981b4737760e4608b3c8fd3ba5404a2c1dd1
marco-benedetto <marco.benedetto93@gmail.com>
2018-08-09 10:21:02 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,82 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+//Add some middleware
+app.use(express.static(__dirname + '/public')); 
+
+//we can create a middleware that tells us how the server is working
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next(); //otherwise the program doesn't go ahead
+});
+
+//app.use((req, res, next) => {
+//    res.render('maintenance.hbs', {
+//        pageTitle: 'Maintenance page'
+//    });
+    //The app doesn't go ahead anymore and diplays only maintenance page
+    //only help page seems not to be affected by this change
+    //this is because the middlewares we're implementing follow the order and .use(express.static(..)) is above of this
+
+//});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+//we can register a handler for http request
+//Route setup
+app.get('/', (req, res) => {
+    //request contains path, body, header
+    //with respond you can send data back
+
+    //res.send('<h1>Hello Express!</h1>');
+
+    //How to send a JSON
+    //res.send({
+    //    name: 'Marco',
+    //    likes: [
+    //        'thing1',
+    //        'thing2'
+    //    ]
+    //});
+
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to some website',
+    });
+});
+
+app.get('/about', (req,res) => {
+    //res.send('About page');
+    res.render('about.hbs', {
+        pageTitle: 'AboutPage',
+    });
+});
+
+app.get('/bad', (req,res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+//now we have to bind a port on the maching and make it listen
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
